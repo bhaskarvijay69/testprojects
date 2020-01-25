dc861bce1f80c283b9daa9197c50d376dff42c3d
bhaskarvijay69 <60061508+bhaskarvijay69@users.noreply.github.com>
2020-01-25 19:40:36 +0530

    C#file

---
+++ MonoTest.cs
@@ -0,0 +1,13 @@
+using System;  
+  
+namespace MonoTest  
+{  
+   class Program  
+   {  
+     static void Main(string[] args)  
+      {  
+          Console.WriteLine("Hai to Mono");  
+          Console.Read();  
+      }  
+   }  
+   }
